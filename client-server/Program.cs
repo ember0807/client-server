bd164de55b82d10a2ded960f0ba9871d14a8d80f
XDavy_la41 <XDavy_la41@top-academy.ru>
2025-01-26 12:53:12 +0300

    Add project files.

---
+++ client-server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+	static void Main(string[] args)
+	{
+		int port = 5000;
+		IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+		TcpListener server = new TcpListener(localAddr, port);
+		server.Start();
+		Console.WriteLine("Сервер запущен. Ожидание подключения...");
+
+		using (TcpClient client = server.AcceptTcpClient())
+		{
+			Console.WriteLine("Клиент подключился.");
+
+			// Выводим IP-адрес и порт клиента
+			IPEndPoint clientEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+			Console.WriteLine($"IP-адрес клиента: {clientEndPoint.Address}, Порт клиента: {clientEndPoint.Port}");
+
+			// Получение сообщения от клиента
+			NetworkStream stream = client.GetStream();
+			byte[] buffer = new byte[256];
+			int bytesRead = stream.Read(buffer, 0, buffer.Length);
+			string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+			Console.WriteLine($"Полученное сообщение: {message}");
+
+			// Закрываем соединение
+			stream.Close();
+		}
+
+		server.Stop();
+	}
+}
---
+++ client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+	static void Main(string[] args)
+	{
+		int port = 5000;
+
+		// Устанавливаем соединение с сервером
+		try
+		{
+			using (TcpClient client = new TcpClient("127.0.0.1", port))
+			{
+				Console.WriteLine("Клиент подключён к серверу.");
+
+				// Ввод сообщения с клавиатуры
+				Console.Write("Введите сообщение для отправки на сервер: ");
+				string message = Console.ReadLine();
+				byte[] data = Encoding.UTF8.GetBytes(message);
+
+				// Отправка сообщения на сервер
+				NetworkStream stream = client.GetStream();
+				stream.Write(data, 0, data.Length);
+				Console.WriteLine("Сообщение отправлено: {0}", message);
+
+				// Закрываем соединение
+				stream.Close();
+			}
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("Ошибка: {0}", e.Message);
+		}
+	}
+}
